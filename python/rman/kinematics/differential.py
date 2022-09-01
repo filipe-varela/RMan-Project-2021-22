de8650fa2964422a6b8ec02a40ac4c730d323980
Filipe Varela <43725698+filipe-varela@users.noreply.github.com>
2022-09-01 14:37:32 +0100

    Organize the python folder structure
    The folder structure is now more similar to a package structure, since
    this allows the project to be installed locally and make the import in
    tests and reports a more doable process than making a relative path to
    import or the commands to run the tests be more concise and less
    confused.
    
    As it goes for the structure itself, the structure has its base on the
    `/rman/` directory, followed by three sub-packages: `/dhc/`, `/kinematic/` and
    `/dynamic/`. In these, the different modules will be developed to enable
    the control of the entire robotic arm. For now, only the dhc and kinematic
    sub-packages are in development, with direct already concluded and
    differential in the testing process.

---
+++ python/rman/kinematics/differential.py
@@ -0,0 +1,33 @@
+from . import dkin
+import sympy as sp
+import symengine as se
+
+def geometric_jacobian(Robot: sp.Matrix, q: sp.Matrix = None) -> sp.Matrix:
+    # variable initialization
+    z0, p0 = sp.Matrix([0, 0, 1]), sp.Matrix([0, 0, 0, 1])
+    q = list(Robot.free_symbols) if q is None else q
+    n_joints = len(q)
+    T = dkin(Robot)
+    pe = T*p0; pe.row_del(-1)
+    J = se.zeros(6, n_joints)
+
+    # first iteration
+    if Robot[0,0] in q:
+        # prismatic joint
+        J[:,0] = se.Matrix([z0, se.zeros(3,1)])
+    else:
+        # revolute joint
+        J[:,0] = se.Matrix([z0.cross(pe), z0])
+
+    # the rest of iterations
+    for joint in range(1,n_joints):
+        A = dkin(Robot[:joint,:])
+        R = A[:3,:3]; zi_1 = R*z0
+        if Robot[joint, 0] in q: 
+            # prismatic joint
+            J[:,joint] = se.Matrix([zi_1, se.zeros(3,1)])
+        else: 
+            # revolute joint
+            pi_1 = A*p0; pi_1.row_del(-1)
+            J[:,joint] = se.Matrix([zi_1.cross(pe-pi_1), zi_1])
+    return sp.nsimplify(se.sympify(J), tolerance=1e-10, rational=False)
---
+++ python/rman/kinematics/direct.py
@@ -0,0 +1,27 @@
+import sympy as sp
+import symengine as se
+from sympy import cos, sin
+
+
+# d v a alpha o
+def dkin(Robot: sp.Matrix) -> sp.Matrix:
+    """The Direct Kinematics function in which
+    outputs the matrix T by a given matrix with
+    the columns being: [d, v, a, alpha, offset].
+    Any column added will be ignored.
+    
+    Returns:
+        T: A simplified version of the direct 
+        kinematic matrix computed by the Robot given.
+    """
+    T = se.eye(4)
+    for row in range(Robot.rows):
+        d, v, a, alpha, o = Robot[row, 0], Robot[row, 1], Robot[row, 2], Robot[row, 3], Robot[row, 4]
+        T = T * se.Matrix([[ cos(v+o), -sin(v+o)*cos(alpha),  sin(v+o)*sin(alpha), a*cos(v+o)],
+                           [ sin(v+o),  cos(v+o)*cos(alpha), -cos(v+o)*sin(alpha), a*sin(v+o)],
+                           [        0,           sin(alpha),           cos(alpha),          d],
+                           [        0,                    0,                    0,          1]])
+    return sp.nsimplify(se.sympify(T), tolerance=1e-10, rational=False)
+    # T = se.sympify(T)
+    # T = sp.Matrix([el.xreplace(Transform(lambda x: round(x, 3), lambda x: isinstance(x, sp.Float))) for row in T for el in row])
+    # return sp.Matrix(T)
